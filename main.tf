67d998008239617bf7ee6a082b61ce0257c9ac3c
bdevops99 <bdevops99@gmail.com>
2023-06-04 01:01:01 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -1,9 +1,50 @@
-resource "aws_instance" "test" {
-  ami = "ami-08ee87f57b38db5af"
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id      = var.vpc_id
 
+  ingress {
+    description      = "App"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_app_cidr
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.bastion_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${var.env}-sg"
+  }
+}
+resource "aws_launch_template" "template" {
+  name_prefix   = "${var.name}-${var.env}"
+  image_id      = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
 }
 
-variable "instance_type" {}
-variable "subnet_id" {}
+resource "aws_autoscaling_group" "asg" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "max_size" {}
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "bastion_cidr" {}
