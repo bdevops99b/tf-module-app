6be2295f5559ad7d52e0917ef87df5a56468efb9
bdevops99 <bdevops99@gmail.com>
2023-06-04 10:49:23 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_autoscaling_group" "asg" {
   dynamic "tag" {
     for_each = local.asg_tags
     content {
-      key                 = tag.value
+      key                 = tag.key
       propagate_at_launch = true
       value               = tag.value
     }
