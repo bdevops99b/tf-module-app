46df35fcc5587d127636504cdc0c4de79f427e3d
bdevops99 <bdevops99@gmail.com>
2023-06-07 11:30:45 +0530

    application load balancer

--- main.tf
+++ main.tf
@@ -85,10 +85,10 @@ resource "aws_lb_listener_rule" "rule" {
   }
 }
 
-#resource "aws_route53_record" "main" {
-#  zone_id = var.domain_id
-#  name    = local.dns_name
-#  type    = "C"
-#  ttl     = 30
-#  records = [var.dns_name]
-#}
+resource "aws_route53_record" "main" {
+  zone_id = var.domain_id
+  name    = local.dns_name
+  type    = "C"
+  ttl     = 30
+  records = [var.dns_name]
+}
