ea0cb1c1db37a4167df2eaed317f9aea1dd54eee
bdevops99 <bdevops99@gmail.com>
2023-06-18 21:37:02 +0530

    Jenkins - pipeline

--- main.tf
+++ main.tf
@@ -52,6 +52,18 @@ resource "aws_launch_template" "template" {
   }))
 }
 
+resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  name                   = "CPULoadDetect"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  policy_type            = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 20.0
+  }
+}
+
 resource "aws_autoscaling_group" "asg" {
   name               = "${var.name}-${var.env}-asg"
   desired_capacity   = var.desired_capacity
